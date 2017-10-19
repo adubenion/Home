81b855f700c5ca7f74e7920da27b8665eccb79be
adubenion <aaron.dubenion@gmail.com>
2017-10-19 18:18:08 -0400

    edit files

--- src/js/Contact.jsx
+++ src/js/Contact.jsx
@@ -6,12 +6,12 @@ export class Contact extends React.Component {
 			<div>
 				<h1> Contact</h1>
 				<p>
-					Reach out! Contact me via email, LinkedIn or GetHub for more information!<br />
+					Reach out! Contact me via email, LinkedIn or GitHub for more information!<br />
 					<br />
 					<br />
 					Email: <a href="mailto:aaron.dubenion@gmail.com">aaron.dubenion@gmail.com</a><br />
 					<br />
-					LinkedIn: <a href="http://linkedin.com/in/aaron-dubenion-aa34b2bb/"><img src="" alt="LinkedIn" /></a><br />
+					LinkedIn: <a href="http://linkedin.com/in/aaron-dubenion/"><img src="" alt="LinkedIn" /></a><br />
 					<br />
 					GetHub: <a href="http://github.com/adubenion/"><img src="" alt="GitHub" /></a>
 				</p>
